69ed492eeba4d8afecf5ca555ae17730a37c703c
aowuor <angelaowuor@gmail.com>
2023-01-13 21:09:06 +0300

    assignment done

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,33 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray);
+  }
+
+  function handleClick(id) {
+    // add to array -----------(1)
+    const newFoodArray = foods.filter((food) => food.id !== id);
+
+    // remove from array ------------(2)
+    // const newFoodArray = foods.filter((food) => food.id !== id);
+    
+    // update element in array ------------(3)
+    // const newFoodArray = foods.map((food) => {
+    //   if(food.id === id){
+    //     return {...food, heatLevel: food.heatLevel + 1};
+    //   }else{
+    //     return food;
+    //   }
+    // });
+    
+    setFoods(newFoodArray);
+    
+    
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
